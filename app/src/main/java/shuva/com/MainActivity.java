1d0fe609c4caeec8d33dbacee3d4bbaa05bcd618
Shuva-Das <shuva.pciu.cse18@gmail.com>
2021-08-31 00:49:13 +0600

    first commit

---
+++ app/src/main/java/shuva/com/MainActivity.java
@@ -0,0 +1,117 @@
+package shuva.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+//import android.widget.GridLayout;
+import androidx.gridlayout.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    //0=Love    1=cross
+    int activePlayer = 0;
+    boolean gameIsActive = true;
+    //2 means unplayed
+    int[] gameState={2,2,2,2,2,2,2,2,2};
+    int[][] winningPositions = {{0,1,2}, {3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
+
+    public void dropIn(View view)
+    {
+        ImageView counter = (ImageView) view;
+       // System.out.println(counter.getTag().toString());
+        int tagCounter = Integer.parseInt(counter.getTag().toString());
+
+        if(gameState[tagCounter]==2 && gameIsActive) {
+            gameState[tagCounter] = activePlayer;
+            counter.setTranslationY(-1000f);
+            if (activePlayer == 0) {
+                counter.setImageResource(R.drawable.o);
+                activePlayer = 1;
+            } else {
+                counter.setImageResource(R.drawable.x);
+                activePlayer = 0;
+            }
+            counter.animate().translationYBy(1000f).rotation(360).setDuration(300);
+
+            for(int[] winningPosition : winningPositions)
+            {
+
+                if(gameState[winningPosition[0]] == gameState[winningPosition[1]] && gameState[winningPosition[1]] == gameState[winningPosition[2]]
+                && gameState[winningPosition[0]] != 2)
+                {
+                    System.out.println(gameState[winningPosition[0]]);
+
+                    //somebody has won
+                    gameIsActive = false;
+                    String winner = "Breakup";
+                    if(gameState[winningPosition[0]]==0)
+                    {
+                        winner = "Love";
+                    }
+
+                    TextView winnerMessage = (TextView) findViewById((R.id.winnerMessage));
+                    winnerMessage.setText(winner + " Has Won!!");
+
+                    LinearLayout layout = (LinearLayout) findViewById(R.id.playAgainLayout);
+                    layout.setVisibility(View.VISIBLE);
+                }else{
+                    boolean gameIsOver = true;
+                    for(int counterState : gameState)
+                    {
+                        if(counterState==2)
+                        {
+                            gameIsOver = false;
+                        }
+                    }
+                    if(gameIsOver)
+                    {
+                        TextView winnerMessage = (TextView) findViewById((R.id.winnerMessage));
+                        winnerMessage.setText("It's Draw!");
+
+                        LinearLayout layout = (LinearLayout) findViewById(R.id.playAgainLayout);
+                        layout.setVisibility(View.VISIBLE);
+
+                    }
+                }
+
+
+
+
+            }
+
+            //Log.i("img", "Click");
+        }
+
+    }
+
+    public void playAgain(View view)
+    {
+        Log.i("Button","Clicked123");
+        gameIsActive = true;
+        LinearLayout layout = (LinearLayout)findViewById(R.id.playAgainLayout);
+        layout.setVisibility(View.INVISIBLE);
+        activePlayer = 0;
+        for(int i=0; i<gameState.length; i++)
+        {
+            gameState[i]=2;
+        }
+
+        GridLayout gridLayout1 = (GridLayout)findViewById(R.id.gridLayout1);
+
+        for(int j=0; j<gridLayout1.getChildCount(); j++) {
+            ((ImageView) gridLayout1.getChildAt(j)).setImageResource(0);
+        }
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
